03a49d7faf33dab500d7a5bc1524fc487d4d968a
6ixfalls <23470032+6ixfalls@users.noreply.github.com>
2022-08-27 23:16:22 -0700

    Parallax intro

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,78 @@
 <template>
-  <router-view class="hidden" />
-  <div
-    id="intro"
-    class="
-      fixed
-      w-screen
-      h-screen
-      flex
-      items-center
-      text-white
-      font-bold
-      text-5xl
-    "
-  >
-    <div id="name" class="">sixfalls</div>
+  <div ref="root" @mousemove="mouseMove">
+    <router-view class="hidden" />
+    <div
+      id="intro"
+      class="
+        fixed
+        w-screen
+        h-screen
+        flex
+        items-center
+        justify-center
+        text-white
+      "
+    >
+      <div
+        id="container"
+        class="flex flex-col w-2/3 h-1/3 perspective-[1800px]"
+      >
+        <div id="name" class="font-bold text-5xl rotate-y-45 translate-z-8">
+          sixfalls
+        </div>
+        <span id="desc" class="font-text text-3xl rotate-y-45 translate-z-5"
+          >Self-taught, game and software developer.</span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import anime from "animejs";
-import Letterize from "letterizejs";
+
+const range = 18;
+
+const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1);
 
 export default defineComponent({
-  mounted: () => {
-    const introName = new Letterize({
-      targets: "#intro #name",
-    });
+  mounted: function () {
+    const introName = this.$refs.root.querySelector("#intro #name");
+    introName.innerHTML = introName.innerHTML
+      .split("")
+      .map((char) => `<span>${char}</span>`)
+      .join("");
 
-    const animation = anime.timeline({
-      targets: introName.listAll,
-      delay: anime.stagger(150),
-    });
+    const introDesc = this.$refs.root.querySelector("#intro #desc");
+    introDesc.innerHTML = introDesc.innerHTML
+      .split(" ")
+      .map((char) => `<span>${char}</span>`)
+      .join(" ");
 
-    animation.add({
+    const introAnimation = anime.timeline();
+
+    introAnimation.add({
+      targets: introName.querySelectorAll("span"),
+      delay: anime.stagger(50),
       opacity: [0, 1],
+      color: "#eee",
     });
+    introAnimation.add({
+      targets: introDesc.querySelectorAll("span"),
+      delay: anime.stagger(200),
+      opacity: [0, 1],
+      color: "#ddd",
+    });
+  },
+  methods: {
+    mouseMove: function ({ x, y }) {
+      const yValue = calcValue(y, window.innerHeight);
+      const xValue = calcValue(x, window.innerWidth);
+
+      const container = this.$refs.root.querySelector("#container");
+      container.style.transform = `translateX(${xValue}px) translateY(${yValue}px)`;
+    },
   },
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require("tailwindcss/plugin")
 
 module.exports = {
   content: [
@@ -12,5 +13,40 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    fontFamily: {
+      text: ['Karla', ...defaultTheme.fontFamily.sans],
+    }
   },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities({
+        'translate-z': (value) => ({
+          '--tw-translate-z': value,
+          transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) rotateY(var(--tw-rotate-y)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+        }),
+      },
+        { values: theme('translate') });
+    }),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities({
+        'rotate-y': (value) => ({
+          '--tw-rotate-y': value,
+          transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) rotateY(var(--tw-rotate-y)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+        }),
+      },
+        { values: theme('rotate') });
+    }),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities({
+        'perspective': (value) => ({
+          '-webkit-perspective': value,
+          '-moz-perspective': value,
+          '-ms-perspective': value,
+          '-o-perspective': value,
+          perspective: value,
+        }),
+      },
+        { values: theme('width') });
+    }),
+  ]
 }
