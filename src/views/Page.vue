4977ba26ed5117f043fbaa374e35d11d536c0141
Six <23470032+6ixfalls@users.noreply.github.com>
2023-06-03 22:48:07 -0700

    Add emojis

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -8,6 +8,8 @@
 <script>
 import { defineComponent } from "vue";
 import { marked } from "marked";
+import twemoji from "twemoji";
+import * as emoji from "node-emoji";
 
 // prettier-ignore
 export default defineComponent({
@@ -17,7 +19,31 @@ export default defineComponent({
     },
     computed: {
         markdown() {
-            const markdown = marked(this.body, {headerIds: false, mangle: false});
+            const markdown = marked(this.body, {
+                headerIds: false, mangle: false, extensions: [
+                    {
+                        name: 'twemoji',
+                        level: 'inline',
+                        start(src) {
+                            return src.indexOf(':');
+                        },
+                        tokenizer(src, _) {
+                            const rule = /^:(\w+):/;
+                            const match = rule.exec(src);
+                            if (match) {
+                                return {
+                                    type: 'twemoji',
+                                    raw: match[0],
+                                    emoji: match[1],
+                                };
+                            }
+                        },
+                        renderer(token) {
+                            return twemoji.parse(emoji.emojify(token.raw), {ext: `.svg`, size: 'svg'});
+                        },
+                    }
+                ]
+            });
             return markdown;
         },
     },
